2e3027939b7ef08e3a61cfbf47d2ad79b59865df
Nicolas Rapedius <Nicolas_Rapedius@gmx.de>
2024-11-18 09:10:17 +0100

    Finished exercise 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Nicolas"
+	lastName := "Rapedius"
+	dayOfBirth := 24
+	monthOfBirth := 04
+	yearOfBirth := 2008
+	numberOfSiblings := 1
+	heightInMeters := 1.71
+	zodiacSign := '\u2649'
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
