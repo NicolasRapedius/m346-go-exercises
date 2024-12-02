d55dea39e011bcf0c633860e7500defdfe8b0aae
Nicolas Rapedius <Nicolas_Rapedius@gmx.de>
2024-12-02 08:08:41 +0100

    finished 2.2

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -5,11 +5,16 @@ import "fmt"
 func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
-	fibs[2] = fibs[0] + fibs[1]
+	for i := 2; i < 5; i++ {
+		fibs[i] = fibs[i-1] + fibs[i-2]
+	}
 	// TODO: correct up to index 4 using direct element access
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
+	fibs = append(fibs, fibs[3]+fibs[4]) // TODO: replace 0 with the next Fibonacci number
 	// TODO: compute three more Fibonacci numbers and append them
-
+	for i := 0; i < 3; i++ {
+		nextFib := fibs[len(fibs)-1] + fibs[len(fibs)-2]
+		fibs = append(fibs, nextFib)
+	}
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
 }
