2c22a01648d221d152d533a4ad5ddda48696cfa6
Nicolas Rapedius <Nicolas_Rapedius@gmx.de>
2024-12-02 08:03:54 +0100

    finished exercise 2.1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   int
+	Month int
+	Year  int
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	Name             FullName
+	BirthDate        BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,22 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		Name: FullName{
+			FirstName: "Nicolas",
+			LastName:  "Rapedius",
+		},
+		BirthDate: BirthDate{
+			Day:   24,
+			Month: 4,
+			Year:  2008,
+		},
+		NumberOfSiblings: 1,   // TODO: adjust
+		ZodiacSign:       '\u2649', // TODO: adjust
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
