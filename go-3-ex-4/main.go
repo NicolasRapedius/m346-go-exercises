e655cf1648324947a03824f9dbf10faabcbad40a
Nicolas Rapedius <Nicolas_Rapedius@gmx.de>
2024-12-09 08:41:22 +0100

    finished exercise 4

--- go-3-ex-4/main.go
+++ go-3-ex-4/main.go
@@ -1,30 +1,36 @@
 package main
 
-import "fmt"
+	import "fmt"
 
-const (
-	Diamonds = '\u25c6' // Karo
-	Spades   = '\u2660' // Pik
-	Clubs    = '\u2663' // Kreuz
-	Hearts   = '\u2665' // Herz
+	const (
+		Diamonds = '\u25c6' // Karo
+		Spades   = '\u2660' // Pik
+		Clubs    = '\u2663' // Kreuz
+		Hearts   = '\u2665' // Herz
 
-	Six   = '\u2465'
-	Seven = '\u2466'
-	Eight = '\u2467'
-	Nine  = '\u2468'
-	Ten   = '\u2469'
-	Jack  = 'J'
-	Queen = 'Q'
-	King  = 'K'
-	Ace   = 'A'
-)
+		Six   = '\u2465'
+		Seven = '\u2466'
+		Eight = '\u2467'
+		Nine  = '\u2468'
+		Ten   = '\u2469'
+		Jack  = 'J'
+		Queen = 'Q'
+		King  = 'K'
+		Ace   = 'A'
+	)	
 
-func main() {
-	suits := []rune{Diamonds, Spades, Clubs, Hearts}
-	ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+	func main() {
+		suits := []rune{Diamonds, Spades, Clubs, Hearts}
+		ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
 
-	// TODO: Loop over suits and ranks to output all combinations.
+		// TODO: Loop over suits and ranks to output all combinations.
+		fmt.Print("Suits: ", string(suits), "\nRanks: ", string(ranks), "\n")
 
-	// TODO: delete this line afterwards
-	fmt.Println(suits, ranks)
-}
+		for _, ranks := range ranks {
+			for _, suits := range suits {
+				fmt.Printf("%c%c\t", suits, ranks)
+			}
+			fmt.Println()
+		}
+		
+	}
