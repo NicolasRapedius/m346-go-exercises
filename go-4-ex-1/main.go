9ce5e68dcc8724a6caa41aba16cf6b94d22797a3
Nicolas Rapedius <Nicolas_Rapedius@gmx.de>
2024-12-09 08:56:57 +0100

    finished exercise 4.1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,52 @@
 package main
 
 // TODO: implement the function computeGrade
+import (
+	"errors"
+	"fmt"
+)
+
+func computeGrade(gotPoints, maxPoints float64) (float64, error) {
+	if gotPoints < 0 || maxPoints <= 0 || gotPoints > maxPoints {
+		return 0, errors.New("ungültige Punktzahlen")
+	}
+
+	percentage := gotPoints / maxPoints
+	grade := 1 + 5*percentage
+
+	return grade, nil
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	if grade, err := computeGrade(17.5, 28.0); err == nil {
+		fmt.Println(grade)
+	} else {
+		fmt.Println(err)
+	}
+
+	if grade, err := computeGrade(25.0, 30.0); err == nil {
+		fmt.Println(grade)
+	} else {
+		fmt.Println(err)
+	}
+
+	if grade, err := computeGrade(0.0, 20.0); err == nil {
+		fmt.Println(grade)
+	} else {
+		fmt.Println(err)
+	}
+
+	if grade, err := computeGrade(-5.0, 20.0); err == nil {
+		fmt.Println(grade)
+	} else {
+		fmt.Println(err)
+	}
+
+	if grade, err := computeGrade(25.0, 20.0); err == nil {
+		fmt.Println(grade)
+	} else {
+		fmt.Println(err)
+	}
 }
+//Nicolas
